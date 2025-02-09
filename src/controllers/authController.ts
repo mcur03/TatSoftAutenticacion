da1d4dc23d15322774b39f7e72493766a7ef258c
formy <formy03081999@gmail.com>
2025-02-09 17:18:35 -0500

    100% de la documentación

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -20,13 +20,13 @@ export const login = async (req: Request, res: Response) => {
     // Verifica si el usuario fue encontrado
     if (!user || !user.contraseña || !user.rol) {
         
-      return res.status(404).json({ message: 'Usuario no encontrado o datos incompletos' });
+      return res.status(404).json({ error: 'Usuario no encontrado o datos incompletos' });
     }
 
     // Verifica la contraseña
     const validPassword = await bcrypt.compare(password, user.contraseña);
     if (!validPassword) {
-      return res.status(401).json({ message: 'Contraseña incorrecta' });
+      return res.status(401).json({ error: 'Contraseña incorrecta' });
     }
     if (!process.env.JWT_SECRET) {
       throw new Error('JWT_SECRET is not defined in environment variables');
@@ -45,12 +45,12 @@ export const login = async (req: Request, res: Response) => {
   } catch (error) {
     if (axios.isAxiosError(error)) {
       if (error.response?.status === 404) {
-        return res.status(404).json({ message: 'El usuario no existe' });
+        return res.status(404).json({ error: 'El usuario no existe' });
       }
-      return res.status(error.response?.status || 500).json({ message: 'Error en el microservicio de usuarios' });
+      return res.status(error.response?.status || 500).json({ error: 'Error en el microservicio de usuarios' });
     }
 
     console.error(error);
-    return res.status(500).json({ message: 'Error en el servidor' });
+    return res.status(500).json({ error: 'Error en el servidor' });
   }
 };
