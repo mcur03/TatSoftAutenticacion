2ca482d86dab69fc2d4749660256e23e142969ae
formy <formy03081999@gmail.com>
2025-02-21 15:56:37 -0500

    Cambios para el despliegue

--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -14,15 +14,23 @@ if (!senderAddress) {
   throw new Error("El remitente (AZURE_SENDER_ADDRESS) no está configurado.");
 }
 
+const emailTemplate = `
+  <body>
+    <div>
+      <h1>Hola</h1>
+      <br/>
+      <p>Tu código de verificación para restablecer la contraseña en TatSoft es: <strong>{{code}}</strong></p>
+    </div>
+  </body>
+`;
 
 export const sendEmail = async (to: string, subject: string, code: string) => {
   console.log('entro a sendEmail');
   
   try {
     // Cargar y compilar la plantilla HTML
-    const templatePath = path.join(__dirname, "../templates/verificationTemplate.html");
-    const source = fs.readFileSync(templatePath, "utf-8");
-    const template = Handlebars.compile(source);
+    //const templatePath = path.join(__dirname, "../templates/verificationTemplate.html");
+    const template = Handlebars.compile(emailTemplate);
     const html = template({ code });
 
     // Configurar el mensaje de correo electrónico
