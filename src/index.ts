8d9e51b128b049831bbc05e42cb8b87f047592d9
formy <formy03081999@gmail.com>
2025-02-21 09:43:55 -0500

    cambios para el despliegue

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { HttpRequest, InvocationContext } from '@azure/functions';
-
-export async function myFunction(req: HttpRequest, context: InvocationContext) {
-    return { body: "Hello, Azure Functions!" };
-}
